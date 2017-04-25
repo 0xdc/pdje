ab4b93b8514cc0142a8742b3a9df4276a97721a8
Daniel Cordero <danielcordero@lavabit.com>
2017-04-25 16:04:10 +0000

    Add some tests for User model

--- pdje/tests.py
+++ pdje/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
 
 # Create your tests here.
+from passlib.hash import sha512_crypt
+
+from .models import *
+
+class UserTestCase(TestCase):
+    def test_password_gets_hashed(self):
+        user = User()
+
+        user.name = "valid@email.address"
+        password = "un-crypted password"
+        user.password = password
+
+        user.save()
+
+        self.assertTrue(sha512_crypt.identify(user.password))
+        self.assertTrue(sha512_crypt.verify(password, user.password))
+
+    def test_password_entered_hashed(self):
+        user = User()
+        user.name = "valid@email.address"
+
+        password = "un-crypted password"
+
+        user.password = sha512_crypt.hash(password)
+        before = user.password
+
+        user.save()
+
+        self.assertEquals(before, user.password)
